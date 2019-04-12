20c27aec54c651bde644eb51ceb99b92ef8a2bf9
Prakashravella <42652316+Prakashravella@users.noreply.github.com>
2019-04-12 14:00:09 -0500

    Float Bollean

---
+++ FloatAndDouble/src/com/prakash/Main.java
@@ -0,0 +1,22 @@
+package com.prakash;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int myInt = 5;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3f;
+        System.out.println("my Int " + myInt);
+        System.out.println("my Int " + myFloatValue);
+        System.out.println("my Int " + myDoubleValue);
+
+
+        double numPounds = 200d;
+        double converKil0 = numPounds * 0.45359237d;
+        System.out.println(converKil0);
+
+        double pi =3.14
+        }
+    }
+
